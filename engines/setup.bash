0d52370235b3ca8cdb89d066a612a8544806aec6
James Vodanovich <james@jvodan.com>
2014-10-13 18:59:49 +1100

    call install.pap

--- engines/setup.bash
+++ engines/setup.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/app/maintence
- php rebuildall.php
+php maintenance/install.php
 
 mkdir images/temp images/archive images/thumbs
 chown og+w images/temp images/archive images/thumbs
