6f4183f9d09efb2b079041a867104497620e4017
James Vodanovich <james@jvodan.com>
2014-11-17 17:56:57 +1100

    fixed setup.bash

--- engines/scripts/setup.bash
+++ engines/scripts/setup.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /home/app/maintence
-php maintenance/install.php
+cd /home/app/
+php maintenance/install.php $user_entered_short_title $wiki_admin_email --pass $admin_pass
 cd /home/app
 mkdir -p images/temp images/archive images/thumbs
 chmod g+w images/temp images/archive images/thumbs
