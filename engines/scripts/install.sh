c7a10d13a7149421c0a48b886e787c4be7bf085c
James Vodanovich <james@jvodan.com>
2015-01-01 14:37:06 +1100

    ini

--- engines/scripts/install.sh
+++ engines/scripts/install.sh
@@ -2,7 +2,7 @@
 cd /home/app/
 php maintenance/install.php --dbname $dbname --dbserver $dbhost  --dbuser $dbuser --dbpass $dbpasswd --server http://$fqdn/   $user_entered_short_title $wiki_admin_email --pass $admin_pass 
 
-php maintenance/update.php --dbname $dbname --dbserver $dbhost  --dbuser $dbuser --dbpass $dbpasswd --server http://$fqdn/   $user_entered_short_title $wiki_admin_email --pass $admin_pass 
+
 
 cd /home/app
 mkdir -p images/temp images/archive images/thumbs
