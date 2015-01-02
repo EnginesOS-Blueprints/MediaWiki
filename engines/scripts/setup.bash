b3b7dc72d6143f5e874c17d0edff1189c645e251
James Vodanovich <james@jvodan.com>
2015-01-02 17:37:38 +1100

    ini

--- engines/scripts/setup.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/app/
-php maintenance/install.php $user_entered_short_title $wiki_admin_email --pass $admin_pass
-cd /home/app
-mkdir -p images/temp images/archive images/thumbs
-chmod g+w images/temp images/archive images/thumbs
-
