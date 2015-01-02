a832fd1de05f968c0e1cd5d1487aa3f374a7b0c8
James Vodanovich <james@jvodan.com>
2015-01-02 15:27:01 +1100

    ini

--- engines/setup.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/app/maintence
-php maintenance/install.php
-cd /home/app
-mkdir -p images/temp images/archive images/thumbs
-chmod g+w images/temp images/archive images/thumbs
-
