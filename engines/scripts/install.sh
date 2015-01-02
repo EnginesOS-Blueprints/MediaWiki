479b1c7b5ea26e7005c022a87240d98bbf398fd9
James Vodanovich <james@jvodan.com>
2015-01-02 16:52:40 +1100

    ini

--- engines/scripts/install.sh
+++ engines/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/app/
-php maintenance/install.php --dbname $dbname --dbserver $dbhost  --dbuser $dbuser --dbpass $dbpasswd --server http://$fqdn/   $user_entered_short_title $wiki_admin_email --pass $admin_pass "Wiki Title" admin
+php maintenance/install.php --scriptpath ./ --dbname $dbname --dbserver $dbhost  --dbuser $dbuser --dbpass $dbpasswd --server http://$fqdn/   $user_entered_short_title $wiki_admin_email --pass $admin_pass "Wiki Title" admin
 
 
 
